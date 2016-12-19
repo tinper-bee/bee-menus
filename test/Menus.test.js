b070ca56fa16dbbe4e27c4ab7d32ee0428238b50
ahua52 <1468492018@qq.com>
2016-12-19 15:11:40 +0800

    完善测试

--- test/Menus.test.js
+++ test/Menus.test.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import {shallow, mount, render} from 'enzyme';
 import {expect} from 'chai';
+import Menus from '../src/index';
+
+describe('Menus 组件测试', function() {
+	
+	it('Menus exist', function() {
+		let menus = shallow(<Menus/>);
+		expect(menus.hasClass('u-menu')).to.equal(true);
+	})
+})
